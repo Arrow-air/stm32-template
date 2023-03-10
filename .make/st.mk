93d27bd3082b02675a5c5d5d7783566e63646eb3
AM Smith <smitham42@gmail.com>
2023-03-10 14:31:55 -0800

    feat: add supporting configs and workflows

--- .make/st.mk
+++ .make/st.mk
@@ -1,3 +1,7 @@
+## DO NOT EDIT!
+# This file was provisioned by Terraform
+# File origin: https://github.com/Arrow-air/tf-github/tree/main/src/templates/stm32-all/.make/st.mk
+#
 BUILD_IMAGE_NAME     ?= arrow-stm32
 BUILD_IMAGE_TAG      ?= latest
 OUTPUTS_PATH        ?= $(SOURCE_PATH)/out
@@ -17,23 +21,28 @@ build_run = docker run \
 build-docker-pull:
 	@echo docker pull -q $(BUILD_IMAGE_NAME):$(BUILD_IMAGE_TAG)
 
-.help-build:
+.help-st:
 	@echo ""
 	@echo "$(SMUL)$(BOLD)$(GREEN)Build Container$(SGR0)"
 	@echo "  $(BOLD)build$(SGR0)       -- Run 'cmake'"
 
 .SILENT: build-docker-pull
 
-build: build-docker-pull
+stm32-build: build-docker-pull
 	@echo "$(CYAN)Running cmake...$(SGR0)"
 	mkdir -p build
-	@$(call build_run,cmake -S. -Bbuild/)
+	@$(call build_run,cmake -B build/)
 	@$(call build_run,cd build && make $(PROJECT_NAME))
 
-flash: build-docker-pull
-	@echo "$(CYAN)Flashing...$(SGR0)"
-	mkdir -p build
-	@$(call build_run,cmake -S. -Bbuild/)
-	@$(call build_run,cd build && make $(PROJECT_NAME))
+# flash: build-docker-pull build
+# 	@echo "$(CYAN)Flashing...$(SGR0)"
+# 	mkdir -p build
+# 	@$(call build_run,cmake -S. -Bbuild/)
+# 	@$(call build_run,cd build && make $(PROJECT_NAME))
 
-all: release
+stm32-test:build-docker-pull
+	@echo "$(YELLOW)Testing framework not implemented. Skipping.$(SGR0)"
+
+stm32-clean: build-docker-pull
+	@echo "$(CYAN)Cleaning project...$(SGR0)"
+	@$(call build_run,cd build && make clean)
